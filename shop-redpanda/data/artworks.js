8e5e8403955078f4cc2783a6e2f91a234303d10b
loudeb <86251202+loudeb@users.noreply.github.com>
2025-02-04 22:00:31 +0000

    updated descriptions for gallery

--- shop-redpanda/data/artworks.js
+++ shop-redpanda/data/artworks.js
@@ -1,255 +1,241 @@
 // data/artworks.js
 export const artworks = [
-    {
-      id: '1',
-      title: 'Fairy Art',
-      description: 'A beautiful sunset over the mountains. asdasd asd asd asd as dasd ',
-      imageUrl: '/images/fairy_art.jpg',
-      storelink: 'https://www.etsy.com/uk/shop/ShopRedPanda?'
-    },
-    {
-      "id": "6",
-      "title": "Amy",
-      "description": "A beautiful piece of artwork titled Amy.",
-      "imageUrl": "/images/Amy.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "7",
-      "title": "Animal Crossing Large",
-      "description": "A beautiful piece of artwork titled Animal Crossing Larg.",
-      "imageUrl": "/images/Animal_Crossing_Larg.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "8",
-      "title": "Artwork 2",
-      "description": "A beautiful piece of artwork titled Artwork 2.",
-      "imageUrl": "/images/Artwork 2.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "9",
-      "title": "Artwork 3",
-      "description": "A beautiful piece of artwork titled Artwork 3.",
-      "imageUrl": "/images/Artwork 3.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "10",
-      "title": "Artwork 5",
-      "description": "A beautiful piece of artwork titled Artwork 5.",
-      "imageUrl": "/images/Artwork 5.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "11",
-      "title": "Artwork",
-      "description": "A beautiful piece of artwork titled Artwork.",
-      "imageUrl": "/images/Artwork.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "12",
-      "title": "Autumn Mae",
-      "description": "A beautiful piece of artwork titled Autumn Mae.",
-      "imageUrl": "/images/Autumn_Mae.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "13",
-      "title": "Beth",
-      "description": "A beautiful piece of artwork titled Beth.",
-      "imageUrl": "/images/Beth.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "14",
-      "title": "Céline Cresswell Workshop Fairy Mae",
-      "description": "A beautiful piece of artwork titled Céline Cresswell Workshop Fairy Mae.",
-      "imageUrl": "/images/Céline_Cresswell_Workshop_Fairy_Mae.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "15",
-      "title": "Clove",
-      "description": "A beautiful piece of artwork titled Clove.",
-      "imageUrl": "/images/Clove.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "16",
-      "title": "Erica",
-      "description": "A beautiful piece of artwork titled Erica.",
-      "imageUrl": "/images/Erica.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "19",
-      "title": "Fairy Art",
-      "description": "A beautiful piece of artwork titled Fairy Art.",
-      "imageUrl": "/images/fairy_art.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "20",
-      "title": "Frieren Print",
-      "description": "A beautiful piece of artwork titled Frieren Print.",
-      "imageUrl": "/images/Frieren_Print.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "21",
-      "title": "Galinda",
-      "description": "A beautiful piece of artwork titled Galinda.",
-      "imageUrl": "/images/Galinda.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "22",
-      "title": "Halloween Mae",
-      "description": "A beautiful piece of artwork titled Halloween Mae.",
-      "imageUrl": "/images/Halloween_Mae.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "23",
-      "title": "Jet",
-      "description": "A beautiful piece of artwork titled Jet.",
-      "imageUrl": "/images/Jet.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "24",
-      "title": "Jo",
-      "description": "A beautiful piece of artwork titled Jo.",
-      "imageUrl": "/images/Jo.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "25",
-      "title": "Large Bunny Witched",
-      "description": "A beautiful piece of artwork titled Large Bunny Witched.",
-      "imageUrl": "/images/Large_Bunny_Witched.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "26",
-      "title": "Little Women",
-      "description": "A beautiful piece of artwork titled Little Women.",
-      "imageUrl": "/images/Little_Women.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "29",
-      "title": "Meg",
-      "description": "A beautiful piece of artwork titled Meg.",
-      "imageUrl": "/images/Meg.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "30",
-      "title": "My Lady Jane",
-      "description": "A beautiful piece of artwork titled My Lady Jane.",
-      "imageUrl": "/images/My_Lady_Jane.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "31",
-      "title": "Odette",
-      "description": "A beautiful piece of artwork titled Odette.",
-      "imageUrl": "/images/Odette.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "32",
-      "title": "Orange Girl",
-      "description": "A beautiful piece of artwork titled Orange Girl.",
-      "imageUrl": "/images/Orange_Girl.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "33",
-      "title": "Pink Lady",
-      "description": "A beautiful piece of artwork titled Pink Lady.",
-      "imageUrl": "/images/Pink_Lady.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "34",
-      "title": "Rainy Mae 1",
-      "description": "A beautiful piece of artwork titled Rainy Mae 1.",
-      "imageUrl": "/images/Rainy_Mae 1.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "35",
-      "title": "Rainy Mae",
-      "description": "A beautiful piece of artwork titled Rainy Mae.",
-      "imageUrl": "/images/Rainy_Mae.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "37",
-      "title": "Small Shell Mae",
-      "description": "A beautiful piece of artwork titled Small Shell Mae.",
-      "imageUrl": "/images/Small_Shell_Mae.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "38",
-      "title": "Untitled Artwork 1",
-      "description": "A beautiful piece of artwork titled Untitled Artwork 1.",
-      "imageUrl": "/images/Untitled_Artwork 1.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "39",
-      "title": "Untitled Artwork 4",
-      "description": "A beautiful piece of artwork titled Untitled Artwork 4.",
-      "imageUrl": "/images/Untitled_Artwork 4.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "40",
-      "title": "Untitled Artwork 6",
-      "description": "A beautiful piece of artwork titled Untitled Artwork 6.",
-      "imageUrl": "/images/Untitled_Artwork 6.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "41",
-      "title": "Untitled Artwork 7",
-      "description": "A beautiful piece of artwork titled Untitled Artwork 7.",
-      "imageUrl": "/images/Untitled_Artwork 7.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "42",
-      "title": "Waterfall",
-      "description": "A beautiful piece of artwork titled Waterfall.",
-      "imageUrl": "/images/Waterfall.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "43",
-      "title": "Website Gallery Scroll Images",
-      "description": "A beautiful piece of artwork titled Website Gallery Scroll Images.",
-      "imageUrl": "/images/Website_Gallery_Scroll_Images.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "44",
-      "title": "Wicked",
-      "description": "A beautiful piece of artwork titled Wicked.",
-      "imageUrl": "/images/wicked.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    },
-    {
-      "id": "45",
-      "title": "Zelda Colour",
-      "description": "A beautiful piece of artwork titled Zelda Colour.",
-      "imageUrl": "/images/Zelda_Colour.jpg",
-      "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
-    }    
-  ]
+  {
+    "id": "42",
+    "title": "Waterfall Mae",
+    "description": "Mae's quiet place, surrounded by statues of ethereal angel statues.",
+    "imageUrl": "/images/Waterfall.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "14",
+    "title": "Workshop Mae",
+    "description": "Original character Mae, in her workshop",
+    "imageUrl": "/images/Céline_Cresswell_Workshop_Fairy_Mae.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "34",
+    "title": "Rainy Mae",
+    "description": "Mae and her woodland friends take shelter from the storm.",
+    "imageUrl": "/images/Rainy_Mae 1.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "37",
+    "title": "Sleeping Mae",
+    "description": "Mae's imagery is associated with pearls, she sleeps in an oyster shell.",
+    "imageUrl": "/images/Small_Shell_Mae.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "22",
+    "title": "Halloween Mae",
+    "description": "Mae gives out treats to the raccoon trick or treaters",
+    "imageUrl": "/images/Halloween_Mae.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": '1',
+    "title": 'Silvermist',
+    "description": 'Fanart illustration of my favourite fairy character.',
+    "imageUrl": '/images/fairy_art.jpg',
+    "storelink": 'https://www.etsy.com/uk/shop/ShopRedPanda?'
+  },
+  {
+    "id": "21",
+    "title": "Glinda",
+    "description": "Laa laaaa laa la",
+    "imageUrl": "/images/Galinda.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "11",
+    "title": "Rosetta's Garden",
+    "description": "If a flower fairy had a garden, what would it look like?",
+    "imageUrl": "/images/Artwork.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "30",
+    "title": "My Lady Jane",
+    "description": "One of my most popular pieces, illustration of My Lady Jane's wedding scene",
+    "imageUrl": "/images/My_Lady_Jane.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "12",
+    "title": "Autumn Mae",
+    "description": "My original character, Mae, in Autumn.",
+    "imageUrl": "/images/Autumn_Mae.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "25",
+    "title": "Halloween Bunny Mae and Cali",
+    "description": "Mae and Cali dressed as matching bunnies for Halloween",
+    "imageUrl": "/images/Large_Bunny_Witched.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "31",
+    "title": "Odette",
+    "description": "Barbie Odette",
+    "imageUrl": "/images/Odette.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "44",
+    "title": "Wicked",
+    "description": "From L to R: Fiyero, Elphaba, Glinda",
+    "imageUrl": "/images/wicked.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "45",
+    "title": "TOTK",
+    "description": "Tears of the Kingdom illustration",
+    "imageUrl": "/images/Zelda_Colour.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "15",
+    "title": "Clove",
+    "description": "Valorant's Clove",
+    "imageUrl": "/images/Clove.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "23",
+    "title": "Jett",
+    "description": "Valorant's Jett",
+    "imageUrl": "/images/Jet.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "8",
+    "title": "Hera",
+    "description": "from LOTR: The War of The Rohirrim",
+    "imageUrl": "/images/Artwork 2.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "38",
+    "title": "Hera at Helm's Deep",
+    "description": "LOTR: The War of the Rohirrim",
+    "imageUrl": "/images/Untitled_Artwork 1.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "7",
+    "title": "Christmas Market",
+    "description": "What would a Christmas market look like in Animal Crossing?",
+    "imageUrl": "/images/Animal_Crossing_Larg.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "9",
+    "title": "Ma Meillure Enemie",
+    "description": "Sketch from the series Arcane",
+    "imageUrl": "/images/Artwork 3.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "16",
+    "title": "Erica",
+    "description": "Barbie princess illustration",
+    "imageUrl": "/images/Erica.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "33",
+    "title": "Anneliese",
+    "description": "Barbie Princess",
+    "imageUrl": "/images/Pink_Lady.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "20",
+    "title": "Frieren",
+    "description": "Illustration of anime character Frieren",
+    "imageUrl": "/images/Frieren_Print.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "26",
+    "title": "The March Sisters",
+    "description": "From L to R: Jo, Beth, Meg, Amy",
+    "imageUrl": "/images/Little_Women.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "6",
+    "title": "Amy",
+    "description": "Little Women's Beth March",
+    "imageUrl": "/images/Amy.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "24",
+    "title": "Jo",
+    "description": "Little Women's Jo March",
+    "imageUrl": "/images/Jo.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "29",
+    "title": "Meg",
+    "description": "Little Women's Meg March",
+    "imageUrl": "/images/Meg.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "13",
+    "title": "Beth",
+    "description": "Little Women's Beth March",
+    "imageUrl": "/images/Beth.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "32",
+    "title": "Orange Girl",
+    "description": "The first digital art piece I was most proud of.",
+    "imageUrl": "/images/Orange_Girl.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "39",
+    "title": "Mona",
+    "description": "Genshin Impact's Mona",
+    "imageUrl": "/images/Untitled_Artwork 4.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "10",
+    "title": "Koromi Mermaid",
+    "description": "Genshin Impact's Koromi as a mermaid for Mermay",
+    "imageUrl": "/images/Artwork 5.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "40",
+    "title": "NEW JEANS fanart",
+    "description": "",
+    "imageUrl": "/images/Untitled_Artwork 6.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "41",
+    "title": "Jett's Ramen Shop",
+    "description": "What would a ramen shop look like in Valorant?",
+    "imageUrl": "/images/Untitled_Artwork 7.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  },
+  {
+    "id": "43",
+    "title": "Website Gallery Scroll Images",
+    "description": "A beautiful piece of artwork titled Website Gallery Scroll Images.",
+    "imageUrl": "/images/Website_Gallery_Scroll_Images.jpg",
+    "storelink": "https://www.etsy.com/uk/shop/ShopRedPanda?"
+  }
+]
